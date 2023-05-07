8fc1a0ac55418b7f3ac2689c571c8ef7643cd192
maksim <maksimazyr@gmail.com>
2023-05-07 13:39:47 +0300

    feat:add messages in 02-write-file

--- 02-write-file/index.js
+++ 02-write-file/index.js
@@ -2,14 +2,19 @@ const fs = require('fs');
 const process = require('process');
 const { stdin, stdout } = process;
 const path = require('path');
+const { Console } = require('console');
 let writeableStream = fs.createWriteStream(path.join(__dirname, 'text.txt'));
+console.log("Здравствуйте, введите текст");
 stdin.on('data', (data) => {
     if (data.toString().trim() == 'exit') {
+        console.log("Вы вышли из программы");
         process.exit();
     }
+    console.log("продолайте вводить текст");
     writeableStream.write(data);
 });
 process.stdin.resume();
 process.on('SIGINT', () => {
+    console.log("Вы вышли из программы");
     process.exit();
 });
